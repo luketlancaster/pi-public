aab100aa561ad892705a3eaed3a46fbd83e136b7
Luke Lancaster <luketlancaster@gmail.com>
2015-04-06 11:25:18 -0500

    add message for player death on boss lvl

--- coffeedude/js-uncompressed/boss.js
+++ coffeedude/js-uncompressed/boss.js
@@ -190,7 +190,7 @@
       jwbBounceCount = 0;
     }
 
-    if (game.time.now > bulletTime && jwbHitCount <   3) {
+    if (game.time.now > bulletTime && jwbHitCount < 3 && hitCount >= 0) {
       bullet = bullets.getFirstExists(false);
       if(bullet) {
         bullet.reset(jwb.x, jwb.y);
@@ -283,6 +283,11 @@
     this.game.state.start('finalMenu');
   }
 
+  function restartGame() {
+    this.game.state.start('menu');
+    hitCount = 2;
+  }
+
   function killBowtie(bowtie, layer) {
     bowtie.kill();
   }
@@ -301,8 +306,14 @@
   }
 
   function gameOver () {
-    game.state.start('menu');
-    hitCount = 2;
+    jwb.body.velocity.x = 0;
+    jwb.body.velocity.y = 0;
+    text = game.add.text(0, 0, "Better luck next time, Mr. 'President'!", style);
+    text.anchor.set(0.5);
+    text.x = Math.floor(jwb.x + jwb.width / 2) - 100;
+    text.y = Math.floor(jwb.y + jwb.height / 2);
+    victoryButton = game.add.button(300, 150, 'reStart', restartGame, this);
+    victoryButton.scale.setTo(.5);
   }
 
   function render() {
