c087d31adda24c4ba9c985d6ecd04477f1345f54
Luke Lancaster <luketlancaster@gmail.com>
2015-05-08 10:29:27 -0500

    camera fixed, game fixed

---
+++ coffeedude/js-uncompressed/finalMenu.js
@@ -0,0 +1,26 @@
+(function() {
+  game.state.add('finalMenu', {create:create});
+  var startButton,
+      enterKey;
+
+  function create(){
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.add.sprite(0, 0, 'finalMenu');
+    startButton = game.add.button(300, 400, 'reStart', backToStart, this);
+    startButton.scale.setTo(.5);
+
+    enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+    // enterKey.onDown.add(startLvl2);
+  }
+
+  function backToStart () {
+    this.game.state.start('menu');
+  }
+
+  // function startLvl2(){
+  //   game.state.start('boss');
+  // }
+
+
+})();
