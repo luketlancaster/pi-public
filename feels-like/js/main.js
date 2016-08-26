612a10f3dd45998b0b6841e3e4cc5204f602e23a
Luke Lancaster <luketlancaster@gmail.com>
2016-08-26 16:44:54 -0500

    moves REST call to php and removes api key

--- feels-like/js/main.js
+++ feels-like/js/main.js
@@ -1,68 +1,26 @@
 $(function(){
 
-  var error,
-      options,
-      success,
-      url = "http://api.wunderground.com/api/db432a740561cd8d/forecast/geolookup/conditions",
-      response;
-
-  options = {
+  var options = {
     timeout: 5000,
     maximumAge: 0
   };
-  $('#send-it').click(function () {
-    console.log('clicked');
 
-    var lat = 1;
-    var lon = 2;
-
-    $.post('../temp.php', {lat: lat, lon: lon}, function (data) {
-      console.log('back');
-      console.log(data);
-    }, 'json');
-
-    // $.ajax({
-    //   url: 'https://luketlancaster.com/temp.php',
-    //   type: 'POST',
-    //   dataType: 'json',
-    //   data: {lat: lat, lon: lon},
-    // })
-    // .done(function(data) {
-    //   display(data);
-    // });
-  })
-
-  success = function(pos) {
+  var success = function(pos) {
     var lat = pos.coords.latitude;
     var lon = pos.coords.longitude;
-    // ajax post to the php instead
-    // $.ajax({
-    //   url: 'https://luketlancaster.com/temp.php',
-    //   type: 'POST',
-    //   dataType: 'json',
-    //   data: {lat: lat, lon: lon},
-    // })
-    // .done(function(data) {
-    //   display(data);
-    // });
 
-    $.getJSON( url + "/q/" + lat + "," + lon + ".json", function( data ) {
-      response = data;
-    })
-    .done(function() {
-      display(response);
-    })
+    $.post('../../temp.php', {lat: lat, lon: lon}, function (data) {
+      display(data);  
+    }, 'json');
   };
 
-  error = function(err) {
+  var error = function(err) {
     console.warn("ERROR(" + err.code + "): " + err.message);
   };
 
   navigator.geolocation.getCurrentPosition(success, error, options);
 
-  function display(data) {
-      console.log(data);
-      var temp = data.current_observation.feelslike_f;
+  function display(temp) {
       $("#iconBox").toggleClass("hidden");
       $("#yesOrNo").text(temp);
     }
--- temp.php
+++ temp.php
@@ -1,29 +1,22 @@
 <?php
-//store this in the env???
-$api_key = 'db432a740561cd8d';
-$url = "http://api.wunderground.com/api/".$api_key."/conditions";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
      // The request is using the POST method
-    return feels_like_temp($_POST['lat'], $_POST['lon']);
+  return feels_like_temp($_POST['lat'], $_POST['lon']);
 }
 
-//if type==post, do some function?
+
 function feels_like_temp($lat, $lon) {
 
-    return json_encode(array("ret" => "you got it dude", $lat, $lon));
+  $api_key = getenv('WUNDERGROUND_API_KEY'); 
+  $url = "http://api.wunderground.com/api/".$api_key."/conditions";
+  $url = $url."/q/".$lat.",".$lon.".json";
 
-    // $url = $url."/q/".$lat.",".$lon.".json";
-    // $request = new HttpRequest($url, HttpRequest::METH_GET);
-    
-    // try {
-    //     $request->send();
-    //     if ($request->getResponseCode() == 200) {
-    //         // gotta make sure it's actually json
-    //         $response = json_decode($request->getResponseBody()));
-    //         // if it is, pull the 'feels like' temp and return it
-    //     }
-    // } catch (HttpException $ex) {
-    //     echo $ex;
-    // }
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_URL,$url);
+  $content = curl_exec($ch);
+  $content = json_decode($content, true);
+  echo $content['current_observation']['feelslike_f'];
+
 }
